37dcb85d67da3ed48e0fb4119578e2b53a1a2ecb
VisualSJ <itharbors@outlook.com>
2014-06-07 22:20:25 +0800

    remove listen ip address

--- start.js
+++ start.js
@@ -8,14 +8,14 @@ var Listen = {
         vhost.addDomain('code.itharbors.com', require("./vhost/code/server"));
         vhost.addDomain('www.itharbors.com', require("./vhost/www/server"));
         vhost.addDomain('www.itharbors.com', require("./vhost/tools/server"));
-        harbors.Server.create('http', '127.0.0.1', 80, vhost);
+        harbors.Server.create('http', 80, vhost);
     },
     3000: function(){
         var vhost = harbors.VHost.create();
         vhost.addDomain('code.itharbors.com', require("./vhost/code/server"));
         vhost.addDomain('www.itharbors.com', require("./vhost/www/server"));
         vhost.addDomain('tools.itharbors.com', require("./vhost/tools/server"));
-        harbors.Server.create('http', '127.0.0.1', 3000, vhost);
+        harbors.Server.create('http', 3000, vhost);
     }
 };
 
