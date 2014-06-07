7e8bfadb1b8150cd91163277e502bee521161cf5
VisualSJ <itharbors@outlook.com>
2014-06-07 20:14:25 +0800

    add tools

--- start.js
+++ start.js
@@ -5,13 +5,16 @@ var server = harbors.Cluster.create();
 var Listen = {
     80: function(){
         var vhost = harbors.VHost.create();
-        vhost.addDomain('code.test.itharbors.com', require("./vhost/code/server"));
-        vhost.addDomain('www.test.itharbors.com', require("./vhost/www/server"));
+        vhost.addDomain('code.itharbors.com', require("./vhost/code/server"));
+        vhost.addDomain('www.itharbors.com', require("./vhost/www/server"));
+        vhost.addDomain('www.itharbors.com', require("./vhost/tools/server"));
         harbors.Server.create('http', '127.0.0.1', 80, vhost);
     },
     3000: function(){
         var vhost = harbors.VHost.create();
         vhost.addDomain('code.test.itharbors.com', require("./vhost/code/server"));
+        vhost.addDomain('www.test.itharbors.com', require("./vhost/www/server"));
+        vhost.addDomain('tools.test.itharbors.com', require("./vhost/tools/server"));
         harbors.Server.create('http', '127.0.0.1', 3000, vhost);
     }
 };
@@ -19,6 +22,6 @@ var Listen = {
 server.setTask('80', Listen['80']);
 server.setTask('3000', Listen['3000']);
 
-server.fork('80');
-server.fork('80');
+//server.fork('80');
+//server.fork('80');
 server.fork('3000');
---
+++ vhost/tools/controller/pull.js
@@ -0,0 +1,15 @@
+var Router = require("../server");
+
+var cp = require("child_process");
+var fs = require("fs");
+var path = require("path");
+
+Router.addRoute('/pull', function(req, res){
+    res.writeHeader(404);
+    res.end('The page you\'re looking for and does not seem to exist.2134');
+    console.log(path.join(__dirname, "../../../../tools"));
+    cp.exec("cd " + path.join(__dirname, "../../../../tools") + " && git pull", function(a, b, c){
+        fs.appendFile("./vhost/tools/Git.log", new Date().toString().substr(0, 25) + c + "\n");
+    });
+
+});
